38bf884d122ccd4cee995e0dbb4a0f561f4b1dac
Dedalus-Stephen <glantonjoejoel@gmail.com>
2020-01-08 21:17:37 +0400

    Update MatchIndex.java

--- MatchIndex.java
+++ MatchIndex.java
@@ -1,4 +1,7 @@
-public class MatchIndex {
+//Problem statement:
+//Given array with values and array with indexes match the values to their indexes.
+
+
     void reorder(int[] arr, int[] ind, int n) {
         int[] result = new int[n];
 
@@ -6,4 +9,4 @@ public class MatchIndex {
             result[ind[i]] = arr[i];
         }
     }
-}
+
